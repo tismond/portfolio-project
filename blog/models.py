75c233734ed945151dd6293ff6727102b888b986
Troy Ismond <tismond@gmail.com>
2019-01-29 20:52:14 +0530

    Blog app added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    Blog_Title = models.Charfield("Troy's Blog!!")
+    pub_date = models.DateField(pub_date)
+    Body = models.Charfield(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    
